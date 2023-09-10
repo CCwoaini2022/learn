158255d0606641bb1cfe25b5d9eff98f2994b329
CCwoaini2022 <116441780+CCwoaini2022@users.noreply.github.com>
2023-09-10 22:37:14 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,48 +1,64 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <math.h>
-#include <Windows.h>
-#include <string.h>
-#include < stdlib.h >
-#include <time.h>
-#include "game.h"
 
-void menu()
-{
-	printf("*****************************\n");
-	printf("*******    1.play    ********\n");
-	printf("*******    0.go      ********\n");
-	printf("*****************************\n");
-}
-void game()
-{
-	//存储数据-二维数组
-	char board[ROW][COL];
-	//初始化棋盘-初始化为空格
-	InitBoard(board, ROW, COL);
-	//打印棋盘
-	DisplayBoard(board, ROW, COL);
-}
+//int main()
+//{
+//
+//	
+//	char* ps = "asdfghj";
+//	char arr[] = "adsgm";
+//
+//	printf("%s\n", ps);
+//	printf("%s\n", arr);
+//
+//	
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char str1[] = "wangbo";
+//	char str2[] = "wangbo";
+//	char *str3 = "wangbo";
+//	char *str4 = "wangbo";
+//
+//	if (str1[]== str2[])
+//		printf("同");
+//	else
+//		printf("不同");
+//
+//
+//
+//	return 0;
+//
+//}
+//int main()
+//{
+//	int a[] = { 1,2,3,4,5 };
+//	int b[] = { 0,2,3,4,5 };
+//	int c[] = { 3,2,3,4,5 };
+//	int* arr[3] = { a,b,c };
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 5; j++)
+//		{
+//			printf("%d ", *(arr[i] + j));		
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
 int main()
 {
-	int input = 0;
-	do
-	{
-		menu();
-		printf("请选择:>");
-		scanf("%d",&input);
-		switch (input)
-		{
-		case 1:
-			game();
-			break;
-		case 0:
-			printf("退出游戏\n");
-			break;
-		default:
-			printf("输入错误，请重新输入\n");
-			break;	
-		}
-	} while (input);
+	double* d[5];
+	double* (*pd)[5] = &d;
+
+	int arr[10];
+	int(*parr)[10] = &(arr);
+
+
+	
 	return 0;
 }
